526ef037c9932c4f40f46b2c60fd8ee4ab1c2e18
Christian Busch <kontakt@christian-busch.com>
2019-01-24 11:20:36 +0100

    init new template, first styling

---
+++ app/components/App.vue
@@ -0,0 +1,50 @@
+<template>
+    <Page backgroundColor="#212b40">
+        <ActionBar title="Welcome to NativeScript-Vue!"/>
+        <TabView android:tabBackgroundColor="#53ba82"
+                android:tabTextColor="#c4ffdf"
+                android:selectedTabTextColor="#ffffff"
+                androidSelectedTabHighlightColor="#ffffff"
+                androidTabsPosition="bottom">
+            <TabViewItem title="Tab 1">
+                <GridLayout columns="*" rows="*">
+                    <Label class="message" :text="msg" col="0" row="0"/>
+                </GridLayout>
+            </TabViewItem>
+            <TabViewItem title="Tab 2">
+                <GridLayout columns="*" rows="*">
+                    <Label class="message" text="Tab 2 Content" col="0" row="0"/>
+                </GridLayout>
+            </TabViewItem>
+            <TabViewItem title="Tab 3">
+                <GridLayout columns="*" rows="*">
+                    <Label class="message" text="Tab 3 Content" col="0" row="0"/>
+                </GridLayout>
+            </TabViewItem>
+        </TabView>
+    </Page>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        msg: 'Hello World!'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #212b40;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #fff;
+    }
+</style>
